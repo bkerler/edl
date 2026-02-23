d054a7fc16e3e30e6b83017e6b5a79c9eaa37f73
Bjoern Kerler <info@revskills.de>
2026-02-23 09:21:12 +0100

    Restructure, add uv support

---
+++ fhloaderparse.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import sys
+from edlclient.Tools.fhloaderparse import main
+
+if __name__ == '__main__':
+    main(sys.argv)
---
+++ qc_diag.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+from edlclient.Tools.qc_diag import main
+
+if __name__ == '__main__':
+    main()
